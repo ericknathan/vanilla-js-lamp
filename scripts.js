1baf8428ea07cb5e36286d88393deb88bd03567a
ericknathan <erick.capito@hotmail.com>
2021-08-25 11:19:45 -0300

    :sparkles: Blinking functionality added

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const lamp = document.getElementById('lamp');
 
 document.getElementById('on').addEventListener('click', () => lampActions.turnOn());
 document.getElementById('off').addEventListener('click', () => lampActions.turnOff());
+document.getElementById('blink').addEventListener('click', () => lampActions.turnBlink());
 lamp.addEventListener('dblclick', () => lampActions.break())
 lamp.addEventListener('mouseenter', () => !lamp.src.includes("quebrada") && lampActions.turnOn())
 lamp.addEventListener('mouseleave', () => !lamp.src.includes("quebrada") && lampActions.turnOff())
@@ -16,6 +17,16 @@ const lampActions = {
     turnOff: () => {
         lampActions.lamp.src = "./public/images/desligada.jpg"
         disableButtons(false, true)
+    },
+    turnBlink: () => {
+        if(document.getElementById('blink').textContent == "Piscar") {
+            document.getElementById('blink').textContent = "Parar"
+            const blink = setInterval(() => {
+                document.getElementById('blink').textContent == "Piscar" && clearInterval(blink)
+                lampActions.lamp.src.includes("desligada") ? lampActions.turnOn() : lampActions.turnOff()
+            }, 200)
+            blink;
+        } else document.getElementById('blink').textContent = "Piscar"
     },
     break: () => {
         lampActions.lamp.src = "./public/images/quebrada.jpg";
