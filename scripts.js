327877a30636116852dc7a8b95ba703cadeaed80
ericknathan <erick.capito@hotmail.com>
2021-09-01 07:51:20 -0300

    🐛 Fixed bugs in blink function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 "use strict"
 const lamp = document.getElementById('lamp');
+let blink;
 
 document.getElementById('on').addEventListener('click', () => lampActions.turnOn());
 document.getElementById('off').addEventListener('click', () => lampActions.turnOff());
@@ -11,30 +12,33 @@ lamp.addEventListener('mouseleave', () => !lamp.src.includes("quebrada") && lamp
 const lampActions = {
     lamp: document.getElementById('lamp'),
     turnOn: () => {
-        lampActions.lamp.src = "./public/images/ligada.jpg"
-        disableButtons(true, false)
+        lampActions.lamp.src = "./public/images/ligada.jpg";
+        disableButtons(true, false, false);
     },
     turnOff: () => {
-        lampActions.lamp.src = "./public/images/desligada.jpg"
-        disableButtons(false, true)
+        lampActions.lamp.src = "./public/images/desligada.jpg";
+        disableButtons(false, true, false);
     },
     turnBlink: () => {
         if(document.getElementById('blink').textContent == "Piscar") {
-            document.getElementById('blink').textContent = "Parar"
-            const blink = setInterval(() => {
-                document.getElementById('blink').textContent == "Piscar" && clearInterval(blink)
-                lampActions.lamp.src.includes("desligada") ? lampActions.turnOn() : lampActions.turnOff()
-            }, 200)
+            document.getElementById('blink').textContent = "Parar";
+            blink = setInterval(() => {
+                document.getElementById('blink').textContent == "Piscar" && clearInterval(blink);
+                lampActions.lamp.src.includes("desligada") ? lampActions.turnOn() : lampActions.turnOff();
+            }, 200);
             blink;
-        } else document.getElementById('blink').textContent = "Piscar"
+        } else document.getElementById('blink').textContent = "Piscar";
     },
     break: () => {
         lampActions.lamp.src = "./public/images/quebrada.jpg";
-        disableButtons(true, true)
+        clearInterval(blink);
+        document.getElementById('blink').textContent = "Piscar";
+        disableButtons(true, true, true);
     }
 }
 
-function disableButtons(onState, offState) {
+function disableButtons(onState, offState, blinkState) {
     document.getElementById('on').disabled = onState;
     document.getElementById('off').disabled = offState;
+    document.getElementById('blink').disabled = blinkState;
 }
