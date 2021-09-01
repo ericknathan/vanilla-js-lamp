942ba3cce27fa93ce77770176aba16d6a125a7c1
ericknathan <erick.capito@hotmail.com>
2021-09-01 08:12:29 -0300

    ♻️ Refactor on lampActions

--- scripts.js
+++ scripts.js
@@ -5,18 +5,18 @@ let blink;
 document.getElementById('on').addEventListener('click', () => lampActions.turnOn());
 document.getElementById('off').addEventListener('click', () => lampActions.turnOff());
 document.getElementById('blink').addEventListener('click', () => lampActions.turnBlink());
-lamp.addEventListener('dblclick', () => lampActions.break())
-lamp.addEventListener('mouseenter', () => !lamp.src.includes("quebrada") && lampActions.turnOn())
-lamp.addEventListener('mouseleave', () => !lamp.src.includes("quebrada") && lampActions.turnOff())
+lamp.addEventListener('dblclick', () => lampActions.break());
+lamp.addEventListener('mouseenter', () => !lamp.src.includes("quebrada") && lampActions.turnOn());
+lamp.addEventListener('mouseleave', () => !lamp.src.includes("quebrada") && lampActions.turnOff());
 
 const lampActions = {
     lamp: document.getElementById('lamp'),
     turnOn: () => {
-        lampActions.lamp.src = "./public/images/ligada.jpg";
+        this.lamp.src = "./public/images/ligada.jpg";
         disableButtons(true, false, false);
     },
     turnOff: () => {
-        lampActions.lamp.src = "./public/images/desligada.jpg";
+        this.lamp.src = "./public/images/desligada.jpg";
         disableButtons(false, true, false);
     },
     turnBlink: () => {
@@ -24,13 +24,13 @@ const lampActions = {
             document.getElementById('blink').textContent = "Parar";
             blink = setInterval(() => {
                 document.getElementById('blink').textContent == "Piscar" && clearInterval(blink);
-                lampActions.lamp.src.includes("desligada") ? lampActions.turnOn() : lampActions.turnOff();
+                this.lamp.src.includes("desligada") ? lampActions.turnOn() : lampActions.turnOff();
             }, 200);
             blink;
         } else document.getElementById('blink').textContent = "Piscar";
     },
     break: () => {
-        lampActions.lamp.src = "./public/images/quebrada.jpg";
+        this.lamp.src = "./public/images/quebrada.jpg";
         clearInterval(blink);
         document.getElementById('blink').textContent = "Piscar";
         disableButtons(true, true, true);
